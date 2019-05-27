2565f89a8e2843111b3bd4ea53657f22504dc92b
JoanCynthia <49479936+JoanCynthia@users.noreply.github.com>
2019-05-27 17:04:53 +0530

    Update app.py

--- Login/app.py
+++ Login/app.py
@@ -7,18 +7,30 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	if not session.get('logged_in'):
-		return render_template('login.html')
+		return render_template('loginTest.html')
 	else:
-		return "Hello Boss!"
+		return render_template('logout.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def do_admin_login():
-	if request.form['password'] == 'password' and request.form['username'] == 'admin':
-		session['logged_in'] = True
-	else:
-		flash('wrong password!')
-		return home()
+        if request.method == 'GET':
+                return home()
+        else:
+                if request.form['s1'] == 'Log in':
+                        if request.form['password'] == 'password' and request.form['username'] == 'admin':
+                                session['logged_in'] = True
+                                return render_template('logout.html')
+                        else:
+                                flash('wrong password!')
+                                #session['logged_in'] = False
+                                return home()
 
+@app.route('/logout', methods=['POST'])
+def do_admin_logout():
+    if request.form['s1'] == 'Log out':
+        session['logged_in'] = False
+        return home()
+    
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
 	app.run(debug=True,host='0.0.0.0', port=4000)
