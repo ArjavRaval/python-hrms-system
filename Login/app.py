76fe2f1bfde578c8cb7be32bfdfca5b75527fd18
1994sen <50980896+1994sen@users.noreply.github.com>
2019-05-27 19:00:27 +0530

    Add files via upload
    sen

--- Login/app.py
+++ Login/app.py
@@ -2,35 +2,53 @@ from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
 
+e_ID=0
+
+""" Get and Post using SQL Querries """
+import mysql.connector
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="HRMS"
+)
+mycursor = mydb.cursor()
+mydb.commit()
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	if not session.get('logged_in'):
-		return render_template('loginTest.html')
+		return render_template('login.html')
 	else:
-		return render_template('logout.html')
+		return "Hello Boss!"
 
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['POST'])
 def do_admin_login():
-        if request.method == 'GET':
-                return home()
+        
+        e_ID = request.form['username']
+        sql = "SELECT `Password` FROM `Admin_login_creds` WHERE `e_ID` = "+ str(e_ID)
+        mycursor.execute(sql)
+        password_from_db=mycursor.fetchone()
+          
+        if password_from_db is None :
+                flash("Wrong credentials!! ","danger")
+                flash(" Invalid username !!","danger")
+                return render_template('login.html')
+        if (request.form['password'] == str(password_from_db[0])) :
+                session['logged_in'] = True
+                return render_template('protected.html')
         else:
-                if request.form['s1'] == 'Log in':
-                        if request.form['password'] == 'password' and request.form['username'] == 'admin':
-                                session['logged_in'] = True
-                                return render_template('logout.html')
-                        else:
-                                flash('wrong password!')
-                                #session['logged_in'] = False
-                                return home()
+                flash("Wrong credentials!! ","danger")
+                flash(" Invalid password!!","danger")
+                return render_template('login.html')
+        
+@app.route('/logout')
+def dropsession():
+    session.clear()
+    return render_template('login.html')
 
-@app.route('/logout', methods=['POST'])
-def do_admin_logout():
-    if request.form['s1'] == 'Log out':
-        session['logged_in'] = False
-        return home()
-    
+        
 if __name__ == "__main__":
 	app.secret_key = os.urandom(12)
-	app.run(debug=True,host='0.0.0.0', port=4000)
+	app.run()
