f1771e0483a567fd5d905eee6b4e7f0b982a3c41
sruthivenkatesan <51071230+sruthivenkatesan@users.noreply.github.com>
2019-05-30 13:55:54 +0530

    Update integration of inset and attendance with flash.py

--- ATTENDANCE_CODE/integration of inset and attendance with flash.py
+++ ATTENDANCE_CODE/integration of inset and attendance with flash.py
@@ -1,6 +1,7 @@
 from flask import Flask,  render_template, request, flash
 import mysql.connector
 
+#Connection to DB
 mydb = mysql.connector.connect(
       host="localhost",
       user="root",
@@ -9,44 +10,40 @@ mydb = mysql.connector.connect(
     )
 mycursor = mydb.cursor()
 
-
+#creating webpage
 app = Flask(__name__)
 app.secret_key = 'my unobvious secret key'
 
+#Routing to webpage
 @app.route('/')
 def index():
     return render_template('atten.html')
 
-
+#getting field values from the form
 @app.route('/hi', methods=["POST"])
 def getvalue():
     ab = request.form['e_id']
     cd = request.form['Date']
     ef = request.form['Punch_In']
     gh = request.form['Punch_Out']
-
-    
+      
+ #inserting field values in DB
     sql = "INSERT INTO `attendance`(`e_id`, `Date`, `Punch_In`, `Punch_Out`) VALUES (%s, %s, %s, %s)"
     val = (ab, cd, ef, gh)
-
-    
     mycursor.execute(sql, val)
-    #insert(ab, cd, ef, gh)
     mydb.commit()
 
-    
+#checking whether values from the form is updated in DB or not
     sql= "SELECT `e_id` FROM `attendance` WHERE `e_id` = " + str(ab)
     mycursor.execute(sql)
     e_id_from_db =  mycursor.fetchone()
-    #mydb.commit()
-
+   
+#flashing success/failure message 
     if (request.form['e_id'] == str(e_id_from_db[0])):
-        flash("Success")
+        flash("Successfully updated")
         #return render_template('atten.html')
     else:
-        flash("Failure")
-
-        
+        flash("Failed to update")
     return render_template('atten.html')
 
 if __name__ == "__main__":
