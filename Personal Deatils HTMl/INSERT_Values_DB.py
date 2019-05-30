0cff0ec3b7205754274011f8face0d592616eddd
VasudevEmmennavar <50980999+VasudevEmmennavar@users.noreply.github.com>
2019-05-30 13:29:48 +0530

    Add files via upload

--- Personal Deatils HTMl/INSERT_Values_DB.py
+++ Personal Deatils HTMl/INSERT_Values_DB.py
@@ -11,8 +11,23 @@ def insertValues(eid, gender, BloodGrp, Addr_Line_1, Addr_Line_2, City, State, P
 
    #insert input data into DB
     sql = "INSERT INTO `per_details`(`e_id`, `Gender`, `Blood_Grp`, `Addr_Line_1`, `Addr_Line_2`, `State`, `City`, `PIN`) VALUES(%s,%s,%s,%s,%s,%s,%s, %s)"
-    val = (eid, gender, BloodGrp, Addr_Line_1, Addr_Line_2, City, State, PIN)
+    val = (eid, gender, BloodGrp, Addr_Line_1, Addr_Line_2,  State, City, PIN)
     mycursor.execute(sql, val)
     mydb.commit()
+    mydb.close()
 
-    print(mycursor.rowcount, "record inserted.")
+def dup_check(eid):
+    mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="vasuvasu",
+            database="hrms"
+        )
+    mycursor = mydb.cursor()
+    sql = "SELECT `e_id` FROM `per_details` WHERE `e_id` =" + str(eid)
+    mycursor.execute(sql)
+    x = mycursor.fetchone()
+    mydb.close()
+    return x
+
+   # print(mycursor.rowcount, "record inserted.")
--- Personal Deatils HTMl/UI_Flask.py
+++ Personal Deatils HTMl/UI_Flask.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
 import INSERT_Values_DB
 
 app = Flask(__name__)
+app.secret_key = b'_vasu2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
@@ -21,12 +22,20 @@ def insert():
         City = request.form['City']
         State = request.form['State']
         PIN = request.form['PIN']
-        INSERT_Values_DB.insertValues(eid, gender, BloodGrp, Addr_Line_1, Addr_Line_2, City, State, PIN)
+        x = INSERT_Values_DB.dup_check(eid)
+        print(x)
+        if(x!=None):
+            flash("Eployee ID Alerady exist", "error")
+            return render_template('Personal_Details.html')
+
+        else:
+            INSERT_Values_DB.insertValues(eid, gender, BloodGrp, Addr_Line_1, Addr_Line_2, City, State, PIN)
+            flash("Data store Successefully ", "error")
+            return render_template('Personal_Details.html')
         ######
         return render_template('Personal_Details.html')
     else:
         return "Nothing"
-
 
 if __name__ == '__main__':
     app.run()
